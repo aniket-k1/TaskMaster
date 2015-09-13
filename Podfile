72b5e355bd8eb0e2bdc51b82216f87edc7aefd15
Aniket Kulkarni <aniketk@umich.edu>
2015-09-12 21:04:00 -0400

    added wit.ai framework

--- Podfile
+++ Podfile
@@ -3,3 +3,4 @@ use_frameworks!
 
 pod 'Firebase', '>= 2.3.3'
 pod 'FirebaseUI', '~> 0.2'
+pod 'Wit', '~> 4.0.0'
--- TaskMaster/AppDelegate.swift
+++ TaskMaster/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Wit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Wit.sharedInstance().accessToken = "KGU3BRZ5IY37QT6R3BNCQ4RB6O7VKW6T"
+        Wit.sharedInstance().detectSpeechStop = WITVadConfig.DetectSpeechStop
         return true
     }
 
